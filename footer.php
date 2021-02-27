44f4f972614ec899a92cb7e690d86749fe56a931
Sjoerd <hello@sjoerdkoelewijn.com>
2021-02-27 19:16:34 +0100

    new design

--- footer.php
+++ footer.php
@@ -3,6 +3,10 @@
 			
 		<?php wp_footer(); ?>
 
+		<a class="footer_logo" href="/wp-admin/post-new.php?post_type=recipes">
+			<img src="<?php echo get_template_directory_uri() . "/images/svg/logo.svg" ?>" alt="logo">
+		</a>
+
 	</body>
 
 
