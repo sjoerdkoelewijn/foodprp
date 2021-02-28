ae37fccd90592ad66a5268318e447e245f2c4686
Sjoerd <hello@sjoerdkoelewijn.com>
2021-02-28 15:19:33 +0100

    footer menu

--- footer.php
+++ footer.php
@@ -5,6 +5,10 @@
 
 		<nav class="footer_menu">
 
+			<a class="home" href="/">
+				<img src="<?php echo get_template_directory_uri() . "/images/svg/home.svg" ?>" alt="home">
+			</a>
+
 			<a class="add_recipe" href="/wp-admin/post-new.php?post_type=recipes">
 				<img src="<?php echo get_template_directory_uri() . "/images/svg/add-button.svg" ?>" alt="add recipe">
 			</a>
